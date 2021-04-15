6711634f2d390c8015a95879c80a6c366a856d78
Matheus Alves <matheus.alves@ccc.ufcg.edu.br>
2021-04-15 04:35:34 -0300

    db

---
+++ src/database/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const config = process.env.NODE_ENV === 'test' ? process.env.DB_URL_TEST : process.env.DB_URL;
+
+mongoose.connect(config, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
