6363e0f0957d7a99a42ab4e67193f52c8efa383d
Igor Franca Gadêlha Filho <igor.filho@ccc.ufcg.edu.br>
2021-04-18 15:47:11 -0300

    Setup fixed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Results from './pages/Results';
+
+function App() {
+  return (
+    <div className='App'>
+      <Results />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Results/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function Results() {
+  return (
+    <div>
+      <h1>Results</h1>
+    </div>
+  );
+}
