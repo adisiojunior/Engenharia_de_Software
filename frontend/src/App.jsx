492352345c68705d63e17c15077ccc99a5603ef7
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-04-25 12:45:57 -0300

    Small fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,30 +6,40 @@ function App() {
     <main className='main'>
       <h1 className='title'>Cadastro do usuário</h1>
       <form className='form'>
-        <label htmlFor='name'>
-          Nome
-          <input type='text' id='name' name='name' />
-        </label>
-        <label htmlFor='last-name'>
-          Sobrenome
-          <input type='text' id='last-name' name='last-name' />
-        </label>
-        <label htmlFor='email'>
-          E-mail
-          <input type='text' id='email' name='email' />
-        </label>
-        <label htmlFor='password'>
-          Senha
-          <input type='password' id='password' name='password' />
-        </label>
-        <label htmlFor='confirm-password'>
-          Confirme a sua senha
-          <input
-            type='password'
-            id='confirm-password'
-            name='confirm-password'
-          />
-        </label>
+        <div>
+          <label htmlFor='name'>
+            Nome
+            <input type='text' id='name' name='name' />
+          </label>
+        </div>
+        <div>
+          <label htmlFor='last-name'>
+            Sobrenome
+            <input type='text' id='last-name' name='last-name' />
+          </label>
+        </div>
+        <div>
+          <label htmlFor='email'>
+            E-mail
+            <input type='text' id='email' name='email' />
+          </label>
+        </div>
+        <div>
+          <label htmlFor='password'>
+            Senha
+            <input type='password' id='password' name='password' />
+          </label>
+        </div>
+        <div>
+          <label htmlFor='confirm-password'>
+            Confirme a sua senha
+            <input
+              type='password'
+              id='confirm-password'
+              name='confirm-password'
+            />
+          </label>
+        </div>
       </form>
       <button type='submit'>Concluir</button>
       <button type='button'>Cadastrar Empresa</button>
