8b459c4d24f0e63d50e54c6f9378aa246dbed4d3
jeron7 <jeron7@pop-os.localdomain>
2021-04-27 15:59:50 -0300

    Added rating router to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
 const { errors } = require('celebrate');
+
+const userRoutes = require('./routes/userRoutes');
+const ratingRoutes = require('./routes/ratingRoutes')
 
 const app = express();
 
 app.use(express.json()); 
 app.use(userRoutes);
+app.use(ratingRoutes);
 app.use(cors());
 app.use(errors());
 
