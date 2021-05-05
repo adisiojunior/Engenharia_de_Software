481bd1ef05d2ec95ef0207dd050f89b7359e1b82
UbuntuTrab <you@example.com>
2021-05-05 05:47:41 -0700

    partial rfs 5 and 10 (update and get place)

--- backend/src/controllers/serviceController.js
+++ backend/src/controllers/serviceController.js
@@ -1,99 +1,159 @@
-const Service = require('../models/Service');
+const Service = require("../models/Service");
 const HttpError = require("../error/http-error");
-const User = require('../models/User');
-require('dotenv/config');
+const User = require("../models/User");
+require("dotenv/config");
 
 module.exports = {
+  async create(req, res) {
+    try {
+      const user = await User.findById(req.userId).select("+password");
 
-    async create(req, res) {
-        try {
+      if (await Service.findOne({ name: req.name, street: req.street })) {
+        return res.status(409).send({ error: `Serviço já existente` });
+      }
 
-            const user = await User.findById(req.userId).select('+password')   
+      const service = await Service.create(req.body);
 
-            if (await Service.findOne({ name: req.name, street: req.street })) {
-                return res.status(409).send({ error: `Serviço já existente` });
-            }
+      user.services.push(service._id);
+      user.save();
 
-            const service = await Service.create(req.body);
-            
-            user.services.push(service._id);
-            user.save();
-
-            return res.send({
-                service
-            });
-
-        }
-            catch (err) {
-                return res.status(400).send({ error: 'Falha no cadastro do serviço ou comércio.' });
-            }        
-        
-    },
-
-    async delete(req, res, next) {
-        try {
-            const user = await User.findById(req.userId).select('+password');
-
-            if (!user) {
-                const error = new HttpError(
-                    "Usuário não existe.",
-                    404,
-                );
-                return next(error);
-            }
-
-            const serviceId = req.params.serviceId;
-
-            if (!user.services.includes(req.params.serviceId)) {
-                const error = new HttpError(
-                    "Tarefa não cadastrada.",
-                    400
-                );
-                return next(error);
-            }
-
-            user.services.splice(user.services.indexOf(serviceId), 1);
-            user.save();
-
-            const service = await Service.findByIdAndDelete(serviceId);
-
-            return res.send({ service });
-        }
-        catch (err) {
-            const error = new HttpError(
-                "Falha ao deletar serviço ou comércio.",
-                400
-            );
-            return next(error);
-        }
-    },
-    async read (req, res, next) {
-        const { limit = 0, offset = 0, category } = req.query;
-
-        try {
-            let results = await Service.find({ category });
-
-            if (!results) {
-                throw new HttpError("Não foi encontrado nenhum serviço", 404);
-            }
-
-            if (offset > results.length) {
-                throw new HttpError("O valor de offset é maior que os resultados encontrados", 406)
-            }
-
-            results = results.slice(offset);
-
-            if (limit > 0) {
-                results = results.slice(0, limit);
-            }
-            
-            return res.status(200).send(results);
-        } catch (error) {
-            if (! error instanceof HttpError) {
-                error = new HttpError(error.message, 500); 
-            }
-            return next(error);
-        }
-
+      return res.send({
+        service,
+      });
+    } catch (err) {
+      return res
+        .status(400)
+        .send({ error: "Falha no cadastro do serviço ou comércio." });
     }
-}
+  },
+
+  async getServiceById(req, res, next) {
+    const serviceId = req.params.pid;
+
+    let service;
+    try {
+      service = await Service.findById(serviceId);
+    } catch (err) {
+      const error = new HttpError(
+        "Ocorreu um erro ao consultar o serviço",
+        500
+      );
+      return next(error);
+    }
+
+    if (!service) {
+      const error = new HttpError(
+        "Não foi possível encontrar um serviço com o ID fornecido",
+        404
+      );
+      return next(error);
+    }
+
+    res.send({ service: service.toObject({ getters: true }) });
+  },
+
+  async updatePlace(req, res, next) {
+    const {
+      name,
+      street,
+      neighborhood,
+      category,
+      description,
+      slogan,
+      cnpj,
+      image,
+    } = req.body;
+    const serviceId = req.params.pid;
+
+    let service;
+    try {
+      service = await Service.findById(serviceId);
+    } catch (err) {
+      const error = new HttpError(
+        "Ocorreu um erro ao atualizar o serviço",
+        500
+      );
+      return next(error);
+    }
+
+    service.name = name;
+    service.street = street;
+    service.neighborhood = neighborhood;
+    service.category = category;
+    service.description = description;
+    service.slogan = slogan;
+    service.cnpj = cnpj;
+    service.image = image;
+
+    try {
+      await service.save();
+    } catch (err) {
+      const error = new HttpError(
+        "Ocorreu um erro ao atualizar o serviço",
+        500
+      );
+      return next(error);
+    }
+
+    res.status(200).send();
+  },
+
+  async delete(req, res, next) {
+    try {
+      const user = await User.findById(req.userId).select("+password");
+
+      if (!user) {
+        const error = new HttpError("Usuário não existe.", 404);
+        return next(error);
+      }
+
+      const serviceId = req.params.serviceId;
+
+      if (!user.services.includes(req.params.serviceId)) {
+        const error = new HttpError("Tarefa não cadastrada.", 400);
+        return next(error);
+      }
+
+      user.services.splice(user.services.indexOf(serviceId), 1);
+      user.save();
+
+      const service = await Service.findByIdAndDelete(serviceId);
+
+      return res.send({ service });
+    } catch (err) {
+      const error = new HttpError("Falha ao deletar serviço ou comércio.", 400);
+      return next(error);
+    }
+  },
+  async read(req, res, next) {
+    const { limit = 0, offset = 0, category } = req.query;
+
+    try {
+      let results = await Service.find({ category });
+
+      if (!results) {
+        throw new HttpError("Não foi encontrado nenhum serviço", 404);
+      }
+
+      if (offset > results.length) {
+        throw new HttpError(
+          "O valor de offset é maior que os resultados encontrados",
+          406
+        );
+      }
+
+      results = results.slice(offset);
+
+      if (limit > 0) {
+        results = results.slice(0, limit);
+      }
+
+      return res.status(200).send(results);
+    } catch (error) {
+      if (!error instanceof HttpError) {
+        error = new HttpError(error.message, 500);
+      }
+      return next(error);
+    }
+  },
+};
--- backend/src/routes/serviceRoutes.js
+++ backend/src/routes/serviceRoutes.js
@@ -1,27 +1,38 @@
-const express = require("express")
-const { celebrate, Segments, Joi } = require('celebrate');
+const express = require("express");
+const { celebrate, Segments, Joi } = require("celebrate");
 
-const serviceController = require('../controllers/serviceController')
-const authMiddleware = require('../middleware/auth');
+const serviceController = require("../controllers/serviceController");
+const authMiddleware = require("../middleware/auth");
 
 const serviceRoutes = express.Router();
 
 // When a error occurs, all erros are returned
-const joiOpts = { abortEarly: false }
+const joiOpts = { abortEarly: false };
 
-serviceRoutes.get('/services', celebrate({
-    [Segments.QUERY]: Joi.object().keys({
+serviceRoutes.get(
+  "/services",
+  celebrate(
+    {
+      [Segments.QUERY]: Joi.object().keys({
         category: Joi.array(),
         limit: Joi.number().min(1).max(15),
         offset: Joi.number().min(0).max(14),
-    })
-}, joiOpts), 
-serviceController.read);
+      }),
+    },
+    joiOpts
+  ),
+  serviceController.read
+);
 
-serviceRoutes.use('/services', authMiddleware);
+serviceRoutes.get("/services/:sid", serviceController.getServiceById);
 
-serviceRoutes.post('/services/register', celebrate({
-    [Segments.BODY]: Joi.object().keys({
+serviceRoutes.use("/services", authMiddleware);
+
+serviceRoutes.post(
+  "/services/register",
+  celebrate(
+    {
+      [Segments.BODY]: Joi.object().keys({
         name: Joi.string().required(),
         category: Joi.array().required(),
         street: Joi.string().required(),
@@ -29,10 +40,34 @@ serviceRoutes.post('/services/register', celebrate({
         description: Joi.string().required(),
         slogan: Joi.string(),
         cnpj: Joi.string(),
-        image: Joi.string()
-    }, )
-}, joiOpts ), serviceController.create);
+        image: Joi.string(),
+      }),
+    },
+    joiOpts
+  ),
+  serviceController.create
+);
 
-serviceRoutes.delete('/services/delete/:serviceId', serviceController.delete);
+serviceRoutes.put(
+  "services/:sid",
+  celebrate(
+    {
+      [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        category: Joi.array().required(),
+        street: Joi.string().required(),
+        neighborhood: Joi.string().required(),
+        description: Joi.string().required(),
+        slogan: Joi.string(),
+        cnpj: Joi.string(),
+        image: Joi.string(),
+      }),
+    },
+    joiOpts
+  ),
+  serviceController.updatePlace
+);
+
+serviceRoutes.delete("/services/delete/:serviceId", serviceController.delete);
 
 module.exports = serviceRoutes;
