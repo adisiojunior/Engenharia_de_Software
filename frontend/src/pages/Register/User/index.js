69893737f945e4b13d7fe0d1b374307d66b8df86
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-05-05 19:09:55 -0300

    Register user other fields

--- frontend/src/pages/Register/User/index.js
+++ frontend/src/pages/Register/User/index.js
@@ -3,28 +3,43 @@ import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Form, FormGroup, Input, Label } from 'reactstrap';
 import api from '../../../services/api';
-// import { login } from '../../../services/auth';
 import { Container, Title, StyledButton, Register, Buttons } from './styles';
 
 export const RegisterUser = () => {
   const history = useHistory();
 
   const [name, setName] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleRegisterUser = async (e) => {
     e.preventDefault();
     if (!name) {
-      toast.error('Informe o nome para realizar o registro');
+      toast.error('Informe seu nome para realizar o registro');
     }
-    try {
-      await api.post('/register', {
-        name,
-      });
-      history.push('/app');
-    } catch (error) {
-      toast.error(
-        'Houve um problema com seu cadastro. Preencha todos os campos corretamente'
-      );
+    if (!lastname) {
+      toast.error('Informe seu sobrenome para realizar o registro');
+    }
+    if (!email) {
+      toast.error('Informe seu e-mail para realizar o registro');
+    }
+    if (!password) {
+      toast.error('Informe uma senha para realizar o registro');
+    } else {
+      try {
+        await api.post('/register', {
+          name,
+          lastname,
+          email,
+          password,
+        });
+        history.push('/app');
+      } catch (error) {
+        toast.error(
+          'Houve um problema com seu cadastro. Tente novamente mais tarde'
+        );
+      }
     }
   };
   return (
@@ -37,7 +52,6 @@ export const RegisterUser = () => {
             <Input
               type='text'
               id='inputName'
-              placheholder='Nome'
               onChange={(nameValue) => {
                 setName(nameValue.target.value);
               }}
@@ -45,23 +59,37 @@ export const RegisterUser = () => {
           </FormGroup>
           <FormGroup>
             <Label>Sobrenome</Label>
-            <Input type='text' id='inputLastname' placheholder='Sobrenome' />
+            <Input
+              type='text'
+              id='inputLastname'
+              onChange={(lastnameValue) => {
+                setLastname(lastnameValue.target.value);
+              }}
+            />
           </FormGroup>
           <FormGroup>
             <Label>E-mail</Label>
-            <Input type='email' id='inputEmail' placheholder='E-mail' />
+            <Input
+              type='email'
+              id='inputEmail'
+              onChange={(emailValue) => {
+                setEmail(emailValue.target.value);
+              }}
+            />
           </FormGroup>
           <FormGroup>
             <Label>Senha</Label>
-            <Input type='password' id='inputPassword' placheholder='Senha' />
+            <Input
+              type='password'
+              id='inputPassword'
+              onChange={(passwordValue) => {
+                setPassword(passwordValue.target.value);
+              }}
+            />
           </FormGroup>
           <FormGroup>
             <Label>Confirme a sua senha</Label>
-            <Input
-              type='password'
-              id='confirmPassword'
-              placheholder='Confirme a sua senha'
-            />
+            <Input type='password' id='confirmPassword' />
           </FormGroup>
           <Buttons>
             <StyledButton type='submit' outline className='w-50'>
