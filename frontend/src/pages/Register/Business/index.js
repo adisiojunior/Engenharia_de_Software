10eafb0b1f3001cd253f87618cd2ba0fa16e1505
gabrielgns <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-05-06 19:15:05 -0300

    Merge develop and register business page first look

---
+++ frontend/src/pages/Register/Business/index.js
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { Form, FormGroup, Input, Label } from 'reactstrap';
+import api from '../../../services/api';
+import { Container, Title, ButtonStyle, Register, FormButton } from './styles';
+
+const RegisterBusiness = () => {
+  const history = useHistory();
+
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('');
+  const [street, setStreet] = useState('');
+  const [neighborhood, setNeighborhood] = useState('');
+  const [description, setDescription] = useState('');
+  const [slogan, setSlogan] = useState('');
+  const [cnpj, setCnpj] = useState('');
+
+  const handleRegisterUser = async (e) => {
+    e.preventDefault();
+    if (
+      !name ||
+      !category ||
+      !street ||
+      !neighborhood ||
+      !description ||
+      !slogan ||
+      !cnpj
+    ) {
+      toast.error(
+        'Informe todos os dados do seu negócio para realizar o cadastro'
+      );
+    } else {
+      try {
+        await api.post('/services/register', {
+          name,
+          category,
+          street,
+          neighborhood,
+          description,
+          slogan,
+          cnpj,
+        });
+        history.push('/app');
+      } catch (error) {
+        toast.error(
+          'Houve um problema com o cadastro do seu negócio. Tente novamente mais tarde'
+        );
+      }
+    }
+  };
+  return (
+    <Container>
+      <Register>
+        <Form onSubmit={handleRegisterUser} className='w-75'>
+          <Title>Cadastro do negócio</Title>
+          <FormGroup>
+            <Label>Nome</Label>
+            <Input
+              type='text'
+              id='inputName'
+              onChange={(nameValue) => {
+                setName(nameValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Categoria</Label>
+            <Input
+              type='text'
+              id='inputCategory'
+              onChange={(categoryValue) => {
+                setCategory(categoryValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Rua</Label>
+            <Input
+              type='street'
+              id='inputStreet'
+              onChange={(streetValue) => {
+                setStreet(streetValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Bairro</Label>
+            <Input
+              type='text'
+              id='inputNeighborhood'
+              onChange={(neighborhoodValue) => {
+                setNeighborhood(neighborhoodValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Descrição</Label>
+            <Input
+              type='text'
+              id='inputDescription'
+              onChange={(descriptionValue) => {
+                setDescription(descriptionValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>Slogan</Label>
+            <Input
+              type='text'
+              id='inputSlogan'
+              onChange={(sloganValue) => {
+                setSlogan(sloganValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label>CNPJ</Label>
+            <Input
+              type='text'
+              id='inputCnpj'
+              onChange={(cnpjValue) => {
+                setCnpj(cnpjValue.target.value);
+              }}
+            />
+          </FormGroup>
+          <FormButton>
+            <ButtonStyle type='submit' outline className='w-10'>
+              Cadastrar
+            </ButtonStyle>
+          </FormButton>
+        </Form>
+      </Register>
+    </Container>
+  );
+};
+
+export default RegisterBusiness;
---
+++ frontend/src/pages/Register/Business/styles.js
@@ -0,0 +1,48 @@
+import { Button } from 'reactstrap';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: auto;
+  height: 100%;
+  background-size: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Register = styled.div`
+  height: 100%;
+  width: 40%;
+  background-color: white;
+  opacity: 70%;
+  padding: 30px;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
+export const ButtonStyle = styled(Button)`
+  outline
+  width: 100%;
+  margin: 1rem;
+  color: #34669A;
+  border-radius: 1rem;
+  border-color: #34669A;
+  :hover {
+    background-color: #34669A;
+    border-color: #34669A;
+  }
+`;
+
+export const FormButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
