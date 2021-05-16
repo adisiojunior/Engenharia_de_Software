74fcff856fc9a7d78ab6093aeca3f99734fd5227
Gabriel Nascimento <gabriel.nascimento.santos@ccc.ufcg.edu.br>
2021-05-16 18:23:17 -0300

    Revert "Lts complete"

--- frontend/src/Components/Loading/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Loader from 'react-loader-spinner';
-
-const Loading = () => {
-  return <Loader type='ThreeDots' color='#34669a' height={100} width={100} />;
-};
-
-export default Loading;
